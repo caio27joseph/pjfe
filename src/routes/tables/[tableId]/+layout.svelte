0e07f7a9d05769ff9ebfac14c6dd01bb82da288c
Caio Joseph <caio27joseph@gmail.com>
2023-11-30 13:36:45 -0300

    Add tables base route layout

---
+++ src/routes/tables/[tableId]/+layout.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import TableLeftBar from './TableLeftBar.svelte';
+
+	export let table = {
+		id: '1',
+		title: 'Mesa 1',
+		imageUrl: '/images/table_placeholder.jpg'
+	};
+	export let libraries = [
+		{
+			id: '1',
+			name: 'Biblioteca 1',
+			articles: [
+				{
+					id: '1',
+					title: 'Artigo 1'
+				}
+			]
+		}
+	];
+</script>
+
+<AppShell>
+	<svelte:fragment slot="sidebarLeft">
+		{#if table}
+			<TableLeftBar {table} {libraries} />
+		{/if}
+	</svelte:fragment>
+
+	<div
+		class="container grid grid-rows-[auto,1fr] py-8 px-6 max-h-screen overflow-hidden min-w-full h-full"
+	>
+		<!-- <Navbar tabs={$tabs} /> -->
+		<div class="content-area grid grid-cols-[1fr,auto,300px] gap-4 items-start overflow-hidden">
+			<slot />
+
+			<div
+				class="divider w-0.5 bg-gradient-to-b from-transparent via-gray-500 to-transparent self-stretch"
+			/>
+			<!-- <Inspector /> -->
+		</div>
+	</div>
+</AppShell>
---
+++ src/routes/tables/[tableId]/TableLeftBar.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+	import TableLeftBar from './TableLeftBar.svelte';
+	import { writable } from 'svelte/store';
+
+	export let table = {
+		id: '1',
+		title: 'Mesa 1',
+		imageUrl: '/images/table_placeholder.jpg'
+	};
+	export let libraries = [
+		{
+			id: '1',
+			name: 'Biblioteca 1',
+			articles: [
+				{
+					id: '1',
+					title: 'Artigo 1'
+				}
+			]
+		}
+	];
+
+	let selectedLibraryId = writable<string | undefined>(libraries?.[0]?.id);
+
+	const tableLeftBarRoutes = ['/tables/[tableId]', '/tables/[tableId]/articles/[articleId]'];
+</script>
+
+<div class="container h-screen w-[300px] overflow-auto bg-surface-600">
+	{#if tableLeftBarRoutes.includes($page.route.id ?? '')}
+		<div>
+			<a
+				class="table-info flex font-bold py-0 !h-44"
+				href="/tables/{table.id}/menu"
+				style="background-image: url({table?.imageUrl || '/images/table_placeholder.jpg'});"
+			>
+				<h1 class="text-2xl text-primary-100 p-3 pt-10 w-full bg-gradient-to-t from-black">
+					{table.title}
+				</h1>
+			</a>
+			<div class="library-hub pt-6 pb-2 px-2">
+				<h1 class="text-primary-500 px-4 text-base">Bibliotecas</h1>
+				<div class="hub-options pt-4">
+					<ListBox>
+						{#each libraries as library, i (library.id)}
+							<ListBoxItem
+								padding="0"
+								bind:group={$selectedLibraryId}
+								name={library.name}
+								value={library.id}
+								active="bg-surface-900"
+							>
+								<div class="flex px-4 py-2 items-center">
+									<!-- <Icon
+										icon="material-symbols:menu-book-outline-rounded"
+										width="20"
+										height="28"
+										class="text-tertiary-500"
+									/> -->
+									<h2 class="ml-1 text-base">{library.name}</h2>
+								</div>
+							</ListBoxItem>
+						{/each}
+					</ListBox>
+				</div>
+			</div>
+			{#each libraries as library}
+				<!-- <Library {table} {library} hidden={library.id !== $selectedLibraryId} /> -->
+			{/each}
+		</div>
+	{:else if $page.route.id === '/tables/[tableId]/menu'}
+		<!-- <TableMenuBar /> -->
+	{:else}
+		...
+	{/if}
+</div>
+
+<style>
+	.table-info {
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center;
+		position: relative;
+		align-items: end;
+	}
+	.library-hub {
+		border-bottom: 0.1px solid #ffffff40;
+	}
+</style>
